c48c3470e1bac43d9df2b7fe5fa1f8787063b6b7
NethanelGuetta <netinello@gmail.com>
2024-08-05 11:23:52 +0300

    finish shoe view

---
+++ Hats&Shoes/Controllers/ShoeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hats_Shoes.DAL;
+using Hats_Shoes.Models;
+using System;
+
+
+namespace Hats_Shoes.Controllers
+{
+    public class ShoeController : Controller
+    {
+        private readonly ILogger<ShoeController> _logger;
+
+        public ShoeController(ILogger<ShoeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //מציג את כל הנעליים
+        public IActionResult Index()
+        {
+            var Shoes = Data.Get.Shoes.ToList();
+            return View(Shoes);
+        }
+    }
+}
